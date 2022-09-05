133634ee64dc0e595cb0c88811d468d6f382f109
BAFFA IT 2 <BAFFA IT 2@BAFFA-IT-2>
2022-09-05 16:14:27 +0600

    First Commit......

---
+++ src/com/solution/OOP/FirstCode.java
@@ -0,0 +1,12 @@
+package com.solution.OOP;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hye guys, Java code is running");
+
+	}
+
+}
